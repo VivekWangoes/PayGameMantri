cf6e8397f973be44b68a58e70dc32a2c5139d900
VivekWangoes <100459728+VivekWangoes@users.noreply.github.com>
2023-10-31 16:26:48 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 PAYMENT_KEY = 'dede2d58-bea3-484d-8be7-c8a99aedf7f9'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_TRUSTED_ORIGINS = ["https://crur.serveo.net", "pay.gamemantri.com"]
+CSRF_TRUSTED_ORIGINS = ["https://crur.serveo.net", "https://pay.gamemantri.com"]
 
 
 CORS_ALLOWED_ORIGINS = [
